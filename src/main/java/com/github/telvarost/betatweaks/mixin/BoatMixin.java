a8fc0a145e18907712b206eb778b3528dea4d97b
telontelvarost <telontelvarost@gmail.com>
2024-01-13 09:49:40 -0500

    ISSUE-7 - Add elevator boats

---
+++ src/main/java/com/github/telvarost/betatweaks/mixin/BoatMixin.java
@@ -0,0 +1,30 @@
+package com.github.telvarost.betatweaks.mixin;
+
+import com.github.telvarost.betatweaks.Config;
+import net.minecraft.entity.Boat;
+import net.minecraft.entity.EntityBase;
+import net.minecraft.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Boat.class)
+abstract class BoatMixin extends EntityBase
+{
+    public BoatMixin(Level level) {
+        super(level);
+    }
+
+    @ModifyConstant(method = "tick", constant = @Constant(doubleValue = 1.0, ordinal = 1))
+    private double dConditionDestroyer(double d)
+    {
+        if (Config.ConfigFields.elevatorBoats)
+        {
+            return Double.MAX_VALUE;
+        }
+        else
+        {
+            return d;
+        }
+    }
+}
